a7a9babe9bf30629f7b2c5a7b85e45ea75a0996a
Smmook <diegobix97@gmail.com>
2022-11-24 12:34:22 +0100

    unnecessary imports removed

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         RSA rsa = new RSA();
-        String nombre = "DIEGO ARENAS SUAREZ";
-        for (int i = 0; i < nombre.length(); i++) {
-            if (nombre.charAt(i) == ' ') {
-                System.out.println();
-                continue;
-            }
-            System.out.print(rsa.encode(nombre.charAt(i)) + " ");
-        }
         int[][] numbers = {
                 {228, 0, 275, 225, 367},
                 {32, 118, 367, 118, 356},
